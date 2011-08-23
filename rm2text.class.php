4d8bf179b510e92e1ea0ba37f9e14bfdc4125754
Fahmi Adam <mohammad@fahmi.my>
2011-08-23 19:51:43 +0800

    Update to version 0.2

--- rm2text.class.php
+++ rm2text.class.php
@@ -10,105 +10,103 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @filesource
- * @copyright     Copyright 2010, Tukangkod. (http://kode.fahmi.my)
- * @link          http://kode.fahmi.my/projects/rm2text
- * @version       v0.1
+ * @copyright     Copyright 2010-2011, tukangkod. (http://kode.fahmi.my)
+ * @link          https://github.com/tukangkod/rm2text
+ * @version       v0.2
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-class rm2text
-{
-    function __construct($amount)
-    {
-        return $this->toRMText($amount);
-    }
+class rm2text {
 
-    private function toRMText($amount)
-    {
-        list($ringgit, $sen) = explode('.', $amount);
-        $isen = $this->getPuluhBelas($sen);
-        
-        $arramount = str_split($ringgit);
-        $arramount = array_reverse($arramount);
+	/**
+	 *
+	 * convert number amount into string
+	 * @param float $amount
+	 * @return string
+	 *
+	 */
+    public function toRMText($amount) {
+		if ($this->isFloat($amount)) {
+    		// get the sen part
+    		list($num, $float) = $this->doPB($amount);
+    	}
+    	// get the ringgit part
+    	$buf = $this->doChange($this->doNum($this->doWord($num))) . ' ringgit';
+    	$sen = trim($this->doChange($this->doNum($this->doWord($float))));
+    	$buf .= ($sen)?' '.$sen.' sen':'';
 
-        $camount = count($arramount);
-        $iamount = $camount-1;
+		return $buf;
+	}
 
-        $iringgit = null;
-        switch($iamount)
-        {
-            case "6":
-                if ($arramount[6] > 0) $iringgit .= $this->nombor($arramount[6]).$this->unit(7).' ';
-            case "5":
-                if ($arramount[5] > 0) $iringgit .= $this->nombor($arramount[5]).$this->unit(6);
-            case "4":
-            case "3":
-                $pbr = (isset($arramount[4])?$arramount[4]:'0').$arramount[3];
-                if ($pbr > 0) $iringgit .= $this->getPuluhBelas($pbr).$this->unit(4);
-                if ($pbr == 0 && $arramount[5] > 0) $iringgit .= $this->unit(4);
-            case "2":
-                if ($arramount[2] > 0) $iringgit .= $this->nombor($arramount[2]).$this->unit(3);
-            case "1":
-            case "0":
-                $pb = (isset($arramount[1])?$arramount[1]:'0').$arramount[0];
-                $iringgit .= $this->getPuluhBelas($pb);
-                break;
-        }
-        return ucwords((($iringgit)?$iringgit.' ringgit ':'').(($isen)?$isen.' '.__('cent', true):''));
-    }
-    
-    private function getPuluhBelas($nombor)
-    {
-        list($puluh, $sa) = str_split($nombor);
-        if ($nombor == 0) return '';
-        else if ($puluh == 0) return $this->nombor($sa);
-        else 
-        {
-            if ($puluh == 1)
-            {
-                if ($sa < 2) return $this->nombor(10).$this->unit(strlen($nombor), $nombor);
-                return (($sa > 0)?$this->nombor($sa):$this->nombor($puluh)).$this->unit(strlen($nombor), $nombor);
-            }
-            else
-            {
-                if ($sa == 0) return $this->nombor($puluh).$this->unit(strlen($nombor), $nombor);
-                else return $this->nombor($puluh).$this->unit(strlen($nombor), $nombor).' '.$this->nombor($sa);
-            }
-        }
-    }
+	/**
+	 *
+	 * Detect if amount having float
+	 * @param float $amount
+	 * @return boolean
+	 *
+	 */
+	private function isFloat($amount) {
+		$pattern = '/[-+]?[0-9]*\.?[0-9]+/';
+		return preg_match($pattern, $amount);
+	}
 
-    private function nombor($nombor)
-    {
-        switch($nombor)
-        {
-            case "0": return ''; break;
-            case "1": return 'satu '; break;
-            case "2": return 'dua '; break;
-            case "3": return 'tiga '; break;
-            case "4": return 'empat '; break;
-            case "5": return 'lima '; break;
-            case "6": return 'enam '; break;
-            case "7": return 'tujuh '; break;
-            case "8": return 'lapan '; break;
-            case "9": return 'sembilan '; break;
-            case "10": return 'se'; break;
-        }
-    }
-    
-    private function unit($count, $arrno=null)
-    {
-        if ($arrno == null) $arrno = array(2, 0);
-        switch($count)
-        {
-            case "7": return "juta "; break;
-            case "6": return "ratus "; break;
-            case "5": return ($arrno[0] == "1")?"belas ":"puluh "; break;
-            case "4": return "ribu "; break;
-            case "3": return "ratus "; break;
-            case "2":
-                if ($arrno[0] == 1) return ($arrno[1] > 0)?"belas ":"puluh ";
-                else if ($arrno[0] != 0) return "puluh ";
-                break;
-        }
-        return "";
-    }
+	/**
+	 *
+	 * Split amount into number and decimal
+	 * @param float $amount
+	 * @return array
+	 */
+	private function doPB($amount) {
+		return preg_split('/[.]+/', $amount);
+	}
+
+	/**
+	 *
+	 * Change subject into word and remove zero value
+	 * @param integer $subject
+	 * @return string
+	 *
+	 */
+	private function doWord($subject) {
+		$pattern = '/([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])([0-9])/i';
+		$replacement = '$1 juta $2 ratus $3 puluh $4 ribu $5 ratus $6 puluh $7';
+		$string = preg_replace($pattern, $replacement, str_pad($subject, 7, '0', STR_PAD_LEFT));
+		return preg_replace('/(0 (juta|ratus|ribu|puluh|belas)|0)/', '', $string);
+	}
+
+	/**
+	 *
+	 * Replace number into word
+	 * @param string $subject
+	 * @return string
+	 *
+	 */
+	private function doNum($subject) {
+        $number = array(1 => 'satu', 2 => 'dua', 3 => 'tiga', 4 => 'empat', 5 => 'lima', 6 => 'enam', 7 => 'tujuh', 8 => 'lapan', 9 => 'sembilan');
+        return str_replace(array_keys($number), array_values($number), $subject);
+	}
+
+	/**
+	 *
+	 * Replace string into readable string
+	 * @param string $subject
+	 * @return string
+	 *
+	 */
+	private function doChange($subject) {
+		$word = array(
+		    'satu ratus' => 'seratus',
+		    'satu puluh' => 'sepuluh',
+			'sepuluh satu' => 'sebelas',
+		    'sepuluh dua' => 'dua belas',
+		    'sepuluh tiga' => 'tiga belas',
+		    'sepuluh empat' => 'empat belas',
+		    'sepuluh lima' => 'lima belas',
+		    'sepuluh enam' => 'enam belas',
+		    'sepuluh tujuh' => 'tujuh belas',
+		    'sepuluh lapan' => 'lapan belas',
+		    'sepuluh sembilan' => 'sembilan belas',
+		    'satu ribu' => 'seribu',
+		);
+		return str_replace(array_keys($word), array_values($word), $subject);
+	}
 }
